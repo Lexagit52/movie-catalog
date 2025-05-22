52aa70e3bb1638af3ac534da9a451d3b9028e0db
Lexagit52 <lehakolomiez29@gmail.com>
2025-05-22 23:22:33 +0500

    ✨ Added basic routing pages and Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <nav className="container mx-auto flex space-x-4">
+        <Link href="/">Главная</Link>
+        <Link href="/movies">Фильмы</Link>
+        <Link href="/movies/add">Добавить фильм</Link>
+        <Link href="/about">О приложении</Link>
+      </nav>
+    </header>
+  );
+}
